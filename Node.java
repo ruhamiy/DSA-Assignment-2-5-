d5af646a2d647881db8017f2de6dd674e3d953ba
Ruhama Ristu <117820906+ruhamiy@users.noreply.github.com>
2024-05-31 10:05:30 +0000

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,45 @@
+package Stack;
+
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class Stack {
+    private Node top;
+
+    public Stack() {
+        this.top = null;
+    }
+    public void push(int data) {
+        Node newNode = new Node(data);
+        if (isEmpty()) {
+            top = newNode;
+        } else {
+            newNode.next = top;
+            top = newNode;
+        }
+    }
+    public int pop() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        int data = top.data;
+        top = top.next;
+        return data;
+    }
+    public int peek() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return top.data;
+    }
+    public boolean isEmpty() {
+        return top == null;
+    }
+}
